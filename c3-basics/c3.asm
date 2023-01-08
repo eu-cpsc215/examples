a7d10dbfa77f074467254234820d63bea0fa636c
Victor Bushong <victorbush@gmail.com>
2023-01-07 21:42:12 -0600

    Begin chapter 3 example project.

---
+++ c3-basics/c3.asm
@@ -0,0 +1,10 @@
+; This is a single line comment. Comments start with a semi-colon and take up the rest of the line.
+
+; External function declarations
+extrn ExitProcess : proc
+
+; Data segment
+.DATA
+
+; Code segment
+.CODE
