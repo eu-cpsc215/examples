0a3b733b96bfcbae2a0c6da9d7b0d0c9f0ddd59f
Victor Bushong <victorbush@gmail.com>
2023-01-18 22:47:20 -0600

    Update signed integer examples.

--- c1-signed-ints/c1-signed-ints.c
+++ c1-signed-ints/c1-signed-ints.c
@@ -1,15 +1,17 @@
+// Gives nice, clear integer types: uint8_t, int8_t, etc.
 #include <stdint.h>
 
 int main(int argc, char** argv)
 {
 	// Observe values in memory window
 
-	uint8_t uint8Val = -1;
+	uint8_t uint8Val = 255;
 	int8_t* int8Ptr = &uint8Val;
 
 	uint32_t uint32Val = 0xFFFFFFFF;
 	int32_t* int32Ptr = &uint32Val;
 
+	uint32Val = 0xFFFF1234;
 	int16_t* int16Ptr = (int16_t*) &uint32Val;
 
 	/*
