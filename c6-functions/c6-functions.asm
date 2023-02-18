18de54da8474d74b8986a22cd9fbe7eb5092e1d0
Victor Bushong <victorbush@gmail.com>
2023-02-18 16:47:12 -0600

    Update some formatting in functions example.

--- c6-functions/c6-functions.asm
+++ c6-functions/c6-functions.asm
@@ -9,9 +9,9 @@
 ; =========================================================
 AsmFunctionExample0 PROC
 
-nop		; This function does nothing
+    nop     ; This function does nothing
 
-ret
+    ret
 
 AsmFunctionExample0 ENDP
 
@@ -24,17 +24,17 @@ AsmFunctionExample0 ENDP
 ; =========================================================
 AsmFunctionExample1 PROC
 
-push rbp				; Callee-saved register
-mov rbp, rsp			; Establish frame pointer
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp                ; Callee-saved register
+    mov rbp, rsp            ; Establish frame pointer
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-mov rax, rcx			; Param 'a' (interger)
-movaps xmm8, xmm1		; Param 'b' (floating point)
-mov rax, r8				; Param 'c' (memory address)
+    mov rax, rcx            ; Param 'a' (interger)
+    movaps xmm8, xmm1       ; Param 'b' (floating point)
+    mov rax, r8             ; Param 'c' (memory address)
 
-; -------------- \/ EPILOGUE \/ -----------------
-pop rbp					; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    pop rbp                 ; Restore callee-saved register
+    ret
 
 AsmFunctionExample1 ENDP
 
@@ -48,50 +48,50 @@ AsmFunctionExample1 ENDP
 ; =========================================================
 AsmFunctionExample2 PROC
 
-push rbp				; Callee-saved register
-push rbx				; Callee-saved register (we'll use RBX within this function)
-sub rsp, 10h			; Allocate space for two 64-bit local variables
-mov rbp, rsp			; Establish frame pointer
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp                ; Callee-saved register
+    push rbx                ; Callee-saved register (we'll use RBX within this function)
+    sub rsp, 10h            ; Allocate space for two 64-bit local variables
+    mov rbp, rsp            ; Establish frame pointer
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-; Here's what the stack should look like right now:
-; 
-; RSP/RBP --> | Allocated for local variable
-;             | Allocated for local variable
-;             | Saved RBX for caller
-;             | Saved RBP for caller
-;             | Return address
-;             | Shadow space
-;             | Shadow space
-;             | Shadow space
-;             | Shadow space
-;             | Parameter 'e' value
-;             | Parameter 'f' value
+    ; Here's what the stack should look like right now:
+    ;
+    ; RSP/RBP --> | Allocated for local variable
+    ;             | Allocated for local variable
+    ;             | Saved RBX for caller
+    ;             | Saved RBP for caller
+    ;             | Return address
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Parameter 'e' value
+    ;             | Parameter 'f' value
 
-; Save register params into shadow space just for fun.
-; This is such a simple function, you wouldn't even need to do this, this is just for demo purposes.
-; This space is available for use as you see fit.
-mov [rbp + 28h], rcx	; Move param 'a' into shadow space (see the stack diagram above to understand RBP + 28h)
-mov [rbp + 30h], rdx	; Move param 'b' into shadow space
-mov [rbp + 38h], r8		; Move param 'c' into shadow space
-mov [rbp + 40h], r9		; Move param 'd' into shadow space
+    ; Save register params into shadow space just for fun.
+    ; This is such a simple function, you wouldn't even need to do this, this is just for demo purposes.
+    ; This space is available for use as you see fit.
+    mov [rbp + 28h], rcx    ; Move param 'a' into shadow space (see the stack diagram above to understand RBP + 28h)
+    mov [rbp + 30h], rdx    ; Move param 'b' into shadow space
+    mov [rbp + 38h], r8     ; Move param 'c' into shadow space
+    mov [rbp + 40h], r9     ; Move param 'd' into shadow space
 
-; Get the remaining parameters from the stack
-mov rax, [rbp + 48h]	; Move param 'e' into RAX
-mov rbx, [rbp + 50h]	; Move param 'f' into RBX
+    ; Get the remaining parameters from the stack
+    mov rax, [rbp + 48h]    ; Move param 'e' into RAX
+    mov rbx, [rbp + 50h]    ; Move param 'f' into RBX
 
-; Add everything up (RAX will hold return value)
-add rax, rbx
-add rax, rcx
-add rax, rdx
-add rax, r8
-add rax, r9
+    ; Add everything up (RAX will hold return value)
+    add rax, rbx
+    add rax, rcx
+    add rax, rdx
+    add rax, r8
+    add rax, r9
 
-; -------------- \/ EPILOGUE \/ -----------------
-lea rsp, [rsp + 10h]	; Free stack-allocated space used by the local variables
-pop rbx					; Restore callee-saved register
-pop rbp					; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    lea rsp, [rsp + 10h]    ; Free stack-allocated space used by the local variables
+    pop rbx                 ; Restore callee-saved register
+    pop rbp                 ; Restore callee-saved register
+    ret
 
 AsmFunctionExample2 ENDP
 
@@ -102,35 +102,35 @@ AsmFunctionExample2 ENDP
 ; =========================================================
 AsmFunctionExample3 PROC
 
-push rbp			; Callee-saved register
-sub rsp, 20h		; Shadow space for any function calls
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp            ; Callee-saved register
+    sub rsp, 20h        ; Shadow space for any function calls
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-mov rcx, 3
-call asmFunctionExample3Helper
+    mov rcx, 3
+    call asmFunctionExample3Helper
 
-; -------------- \/ EPILOGUE \/ -----------------
-lea rsp, [rsp+20h]	; Cleanup allocated space
-pop rbp				; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    lea rsp, [rsp+20h]  ; Cleanup allocated space
+    pop rbp             ; Restore callee-saved register
+    ret
 
 AsmFunctionExample3 ENDP
 
 ; =========================================================
 asmFunctionExample3Helper PROC
 
-mov [rsp + 8h], rcx		; Save register parameter into shadow space
-push rbp				; Callee-saved register
-mov rbp, rsp			; Establish frame pointer
-; -------------- /\ PROLOGUE /\ -----------------
+    mov [rsp + 8h], rcx     ; Save register parameter into shadow space
+    push rbp                ; Callee-saved register
+    mov rbp, rsp            ; Establish frame pointer
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-; We could get the param from rcx directly, but this demonstrates
-; saving off the paramter and coming back later to access it.
-mov rax, [rbp + 10h]
+    ; We could get the param from rcx directly, but this demonstrates
+    ; saving off the paramter and coming back later to access it.
+    mov rax, [rbp + 10h]
 
-; -------------- \/ EPILOGUE \/ -----------------
-pop rbp					; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    pop rbp                 ; Restore callee-saved register
+    ret
 
 asmFunctionExample3Helper ENDP
 
@@ -141,19 +141,19 @@ asmFunctionExample3Helper ENDP
 ; =========================================================
 AsmFunctionExample4 PROC
 
-push rbp				; Callee-saved register
-sub rsp, 20h			; Allocate shadow space for any function calls
-mov rbp, rsp			; Establish frame pointer
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp                ; Callee-saved register
+    sub rsp, 20h            ; Allocate shadow space for any function calls
+    mov rbp, rsp            ; Establish frame pointer
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-mov rax, rcx
-mov rcx, 5
-call rax
+    mov rax, rcx
+    mov rcx, 5
+    call rax
 
-; -------------- \/ EPILOGUE \/ -----------------
-lea rsp, [rsp+20h]		; Cleanup allocated space
-pop rbp					; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    lea rsp, [rsp+20h]      ; Cleanup allocated space
+    pop rbp                 ; Restore callee-saved register
+    ret
 
 AsmFunctionExample4 ENDP
 
@@ -165,26 +165,26 @@ AsmFunctionExample4 ENDP
 ; =========================================================
 AsmFunctionExample5 PROC
 
-push rbp			; Callee-saved register
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp        ; Callee-saved register
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-mov rcx, 1		; Param 'a'
-mov rdx, 2		; Param 'b'
-mov r8, 3		; Param 'c'
-mov r9, 4		; Param 'd'
-push 6			; Param 'f'
-push 5			; Param 'e'
-sub rsp, 20h	; Shadow space
+    mov rcx, 1      ; Param 'a'
+    mov rdx, 2      ; Param 'b'
+    mov r8, 3       ; Param 'c'
+    mov r9, 4       ; Param 'd'
+    push 6          ; Param 'f'
+    push 5          ; Param 'e'
+    sub rsp, 20h    ; Shadow space
 
-call asmFunctionExample5Helper	; After return, expect RAX to have result of 21 (0x15)
+    call asmFunctionExample5Helper	; After return, expect RAX to have result of 21 (0x15)
 
-add rsp, 20h	; Cleanup shadow space
-pop rbx			; Cleanup param 'e'
-pop rbx			; Cleanup param 'f'
+    add rsp, 20h    ; Cleanup shadow space
+    pop rbx         ; Cleanup param 'e'
+    pop rbx         ; Cleanup param 'f'
 
-; -------------- \/ EPILOGUE \/ -----------------
-pop rbp			; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    pop rbp         ; Restore callee-saved register
+    ret
 
 AsmFunctionExample5 ENDP
 
@@ -192,32 +192,32 @@ AsmFunctionExample5 ENDP
 ; Assume function has following params: (int a, int b, int c, int d, int e, int f)
 asmFunctionExample5Helper PROC
 
-push rbp				; Callee-saved register
-mov rbp, rsp			; Establish frame pointer
-; -------------- /\ PROLOGUE /\ -----------------
+    push rbp                ; Callee-saved register
+    mov rbp, rsp            ; Establish frame pointer
+    ; -------------------- /\ PROLOGUE /\ --------------------
 
-; Here's what the stack should look like right now:
-; 
-; RSP/RBP --> | Saved RBP for caller
-;             | Return address
-;             | Shadow space
-;             | Shadow space
-;             | Shadow space
-;             | Shadow space
-;             | Parameter value ('f')
-;             | Parameter value ('e')
+    ; Here's what the stack should look like right now:
+    ; 
+    ; RSP/RBP --> | Saved RBP for caller
+    ;             | Return address
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Shadow space
+    ;             | Parameter value ('f')
+    ;             | Parameter value ('e')
 
-; Let's add up all the params
-mov rax, rcx
-add rax, rdx
-add rax, r8
-add rax, r9
-add rax, [rsp + 30h] ; Param 'e'
-add rax, [rsp + 38h] ; Param 'f'
+    ; Let's add up all the params
+    mov rax, rcx
+    add rax, rdx
+    add rax, r8
+    add rax, r9
+    add rax, [rsp + 30h] ; Param 'e'
+    add rax, [rsp + 38h] ; Param 'f'
 
-; -------------- \/ EPILOGUE \/ -----------------
-pop rbp					; Restore callee-saved register
-ret
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    pop rbp                 ; Restore callee-saved register
+    ret
 
 asmFunctionExample5Helper ENDP
 
